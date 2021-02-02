6e0b357e30ca83226753b03d3785ab5490141464
Viktor <lysenko_v@outlook.com>
2021-02-02 13:04:50 +0200

    add data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
-import App from '../components/App';
+import App from "../components/App";
+import { fetchPlanets } from "../store/actions/planets";
+import { wrapper } from "../store/store";
 
 export default function Home() {
-  return (
-    <App />
-  )
+  return <App  />;
 }
+
+export const getStaticProps = wrapper.getServerSideProps(async ({ store }) => {
+  await store.dispatch(fetchPlanets());
+});
