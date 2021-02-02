14084d0fdd76cfd18561a587ce3288c7b293106c
Viktor <lysenko_v@outlook.com>
2021-02-02 13:04:18 +0200

    delete data

--- components/Planets/Planets.js
+++ components/Planets/Planets.js
@@ -1,79 +1,17 @@
 import styles from "./Planets.module.css";
-import { useEffect, useState } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
+import { useState } from "react";
 
 import Grid from "../Grid";
 import Modal from "../Modal/Modal";
 import PlanetInfoForm from "../PlanetInfoForm/PlanetInfoForm";
-import { fetchPlanets } from "../../store/actions/planets";
-import { useRouter } from "next/router";
 
 function Planets(props) {
   const [modalIsActive, setModalActive] = useState(false);
   const [successOrErrorMessage, setMessage] = useState("");
-  useEffect(() => {
-    if (Object.keys(props.data).length < 1) props.fetchPlanets();
-  }, []);
-  const router = useRouter();
 
-  const data = {
-    header: [
-      "name",
-      "rotation_period",
-      "orbital_period",
-      "diameter",
-      "climate",
-      "gravity",
-      "terrain",
-      "surface_water",
-      "population",
-      "residents",
-      "films"
-    ],
-    values: props.data,
-    actions: [
-      {
-        label: "Add Planet",
-        action: (row) => {
-          setModalActive(true);
-          console.log(`redirect to grid with ${row.films.length} Films`);
-        },
-      },
-      {
-        label: "Go to Residents",
-        action: (row) => {
-          console.log(
-            `redirect to grid with ${row.residents.length} Residents`
-          );
-          const residentsIds = row.residents.map(
-            (residentUrl) => residentUrl.match(/\d+/g)[0]
-          );
-          router.push(`/residents/${residentsIds.join("/")}`);
-          console.log(`redirect to grid with ${row.url} Films`);
-        },
-        title: "residents",
-      },
-      {
-        label: "Go to Films",
-        action: (row) => {
-          const filmIds = row.films.map((filmUrl) => filmUrl.match(/\d+/g)[0]);
-          router.push(`/films/${filmIds.join("/")}`);
-          console.log(`redirect to grid with ${row.url} Films`);
-        },
-        title: "films",
-      },
-      {
-        label: "Planet Info",
-        action: (row) => {
-          router.push(`/planets/${row.url.match(/\d+/g)[0]}`);
-          console.log(`redirect to grid with ${row.url} Films`);
-        },
-      },
-    ],
-  };
+  const data = props.data;
 
-  const onSubmitHandler = (dataFromForm, onSubmitProps) => {
+  const onSubmitHandler = (dataFromForm) => {
     setModalActive(false);
     console.log(dataFromForm);
 
@@ -100,16 +38,4 @@ function Planets(props) {
   );
 }
 
-function mapStateToProps(state) {
-  return {
-    data: state.planets.data,
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    fetchPlanets: bindActionCreators(fetchPlanets, dispatch),
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Planets);
+export default Planets;
