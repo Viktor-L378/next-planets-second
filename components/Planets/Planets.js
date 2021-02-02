2f4bf2c14b9a3061b2f7d9c305b6811ff9bad3be
Viktor <lysenko_v@outlook.com>
2021-02-02 14:02:44 +0200

    add data

--- components/Planets/Planets.js
+++ components/Planets/Planets.js
@@ -1,15 +1,69 @@
-import styles from "./Planets.module.css";
 import { useState } from "react";
+import { useRouter } from "next/router";
 
+import styles from "./Planets.module.css";
 import Grid from "../Grid";
 import Modal from "../Modal/Modal";
 import PlanetInfoForm from "../PlanetInfoForm/PlanetInfoForm";
 
 function Planets(props) {
+  const router = useRouter();
   const [modalIsActive, setModalActive] = useState(false);
   const [successOrErrorMessage, setMessage] = useState("");
 
-  const data = props.data;
+  const data = {
+    header: [
+      "name",
+      "rotation_period",
+      "orbital_period",
+      "diameter",
+      "climate",
+      "gravity",
+      "terrain",
+      "surface_water",
+      "population",
+    ],
+    values: props.data,
+    actions: [
+      {
+        label: "Add Planet",
+        action: (row) => {
+          setModalActive(true);
+          console.log(`redirect to grid with ${row.films.length} Films`);
+        },
+      },
+      {
+        label: "Go to Residents",
+        action: (row) => {
+          console.log(
+            `redirect to grid with ${row.residents.length} Residents`
+          );
+          const residentsIds = row.residents.map(
+            (residentUrl) => residentUrl.match(/\d+/g)[0]
+          );
+          router.push(`/residents/${residentsIds.join("/")}`);
+          console.log(`redirect to grid with ${row.url} Films`);
+        },
+        title: "residents",
+      },
+      {
+        label: "Go to Films",
+        action: (row) => {
+          const filmIds = row.films.map((filmUrl) => filmUrl.match(/\d+/g)[0]);
+          router.push(`/films/${filmIds.join("/")}`);
+          console.log(`redirect to grid with ${row.url} Films`);
+        },
+        title: "films",
+      },
+      {
+        label: "Planet Info",
+        action: (row) => {
+          router.push(`/planets/${row.url.match(/\d+/g)[0]}`);
+          console.log(`redirect to grid with ${row.url} Films`);
+        },
+      },
+    ],
+  };
 
   const onSubmitHandler = (dataFromForm) => {
     setModalActive(false);
