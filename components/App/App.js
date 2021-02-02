164811752b2d23b3282b59ccfccda84bc4be11cf
Viktor <lysenko_v@outlook.com>
2021-02-02 13:04:35 +0200

    add planets data

--- components/App/App.js
+++ components/App/App.js
@@ -1,12 +1,69 @@
-import styles from './App.module.css';
+import styles from "./App.module.css";
+import Planets from "../Planets";
+import { useSelector } from "react-redux";
 
-import Planets from '../Planets';
+function App(props) {
 
-function App() {
+  const planetsData = useSelector(state => state.planets.data);
+  console.log(planetsData)
+  const data = {
+    header: [
+      "name",
+      "rotation_period",
+      "orbital_period",
+      "diameter",
+      "climate",
+      "gravity",
+      "terrain",
+      "surface_water",
+      "population",
+    ],
+    values: planetsData,
+    actions: [
+      {
+        label: "Add Planet",
+        action: (row) => {
+          setModalActive(true);
+          console.log(`redirect to grid with ${row.films.length} Films`);
+        },
+      },
+      {
+        label: "Go to Residents",
+        action: (row) => {
+          console.log(
+            `redirect to grid with ${row.residents.length} Residents`
+          );
+          const residentsIds = row.residents.map(
+            (residentUrl) => residentUrl.match(/\d+/g)[0]
+          );
+          router.push(`/residents/${residentsIds.join("/")}`);
+          console.log(`redirect to grid with ${row.url} Films`);
+        },
+        title: "residents",
+      },
+      {
+        label: "Go to Films",
+        action: (row) => {
+          const filmIds = row.films.map((filmUrl) => filmUrl.match(/\d+/g)[0]);
+          router.push(`/films/${filmIds.join("/")}`);
+          console.log(`redirect to grid with ${row.url} Films`);
+        },
+        title: "films",
+      },
+      {
+        label: "Planet Info",
+        action: (row) => {
+          router.push(`/planets/${row.url.match(/\d+/g)[0]}`);
+          console.log(`redirect to grid with ${row.url} Films`);
+        },
+      },
+    ],
+  };
+
   return (
     <div className={styles.App}>
       <h1>Star Wars Planets</h1>
-      <Planets />
+      <Planets data={data} />
     </div>
   );
 }
